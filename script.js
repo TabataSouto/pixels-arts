4f0256221928863ac71fd0bc79fe5fa2ffc237ce
Tabata <tabatac.souto@gmail.com>
2021-11-22 23:44:07 -0300

    Corrigindo requisito 06: Finalizado; Requisito 12: finalizado

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Adicione à página um quadro de pixels, com 25 pixels.
+// ADICIONR PIXEL'S
 const pixelBoard = document.getElementById('pixel-board');
-
 const lines = 5;
 const coluns = 5;
 for (let index = 1; index <= lines; index += 1) {
@@ -8,21 +7,40 @@ for (let index = 1; index <= lines; index += 1) {
   for (let index2 = 1; index2 <= coluns; index2 += 1) {
     const box = document.createElement('div');
     box.className = 'pixel';
+    box.style.marginBottom = '-4px';
     divLine.appendChild(box);
   }
   pixelBoard.appendChild(divLine);
 }
 
-// Defina a cor preta como cor inicial. Ao carregar a página;
-const myFirstColor = document.getElementById('color1');
-myFirstColor.classList.add('selected'); // adiciona nome de classe sem substituir a anterior, deixando a tag com a classe que já tinha mais as que forem adicionadas.
+// DEFINBIR COR "BLACK" INICIAL  E CORES ALEATÓRIAS
+function generateNewColor() {
+  const aleatNum1 = Math.round(Math.random() * 255);
+  const aleatNum2 = Math.round(Math.random() * 255);
+  const aleatNum3 = Math.round(Math.random() * 255);
+  const newColorAleat = `rgb(${aleatNum1}, ${aleatNum2}, ${aleatNum3})`;
+  return newColorAleat;
+}
 
-// 7 - Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
-const firstColor = document.getElementById('color1');
-const secondColor = document.getElementById('color2');
-const thirdColor = document.getElementById('color3');
-const fourColor = document.getElementById('color4');
+const firstColor = document.querySelector('#color1');
+firstColor.classList.add('selected');
+firstColor.style.backgroundColor = 'black';
+const secondColor = document.querySelector('#color2');
+secondColor.style.backgroundColor = generateNewColor();
+const thirdColor = document.querySelector('#color3');
+thirdColor.style.backgroundColor = generateNewColor();
+const fourthColor = document.querySelector('#color4');
+fourthColor.style.backgroundColor = generateNewColor();
 
+// ADICIONAL: TITULO COM CORES ALEATORIAS (REQUISITO 6 E 12)
+const span1 = document.querySelector('#c1');
+span1.style.color = generateNewColor();
+const span2 = document.querySelector('#c2');
+span2.style.color = generateNewColor();
+const span3 = document.querySelector('#c3');
+span3.style.color = generateNewColor();
+
+// SELECIONAR COR
 function addColor(event) {
   const colorElement = document.querySelector('.selected');
   colorElement.classList.remove('selected');
@@ -32,9 +50,6 @@ function addColor(event) {
 firstColor.addEventListener('click', addColor);
 secondColor.addEventListener('click', addColor);
 thirdColor.addEventListener('click', addColor);
-fourColor.addEventListener('click', addColor);
+fourthColor.addEventListener('click', addColor);
 
-// Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada
-const selectedColor = document.querySelector('.selected');
-
-// Quem tiver a classe selectd pinda a classe pixel;
+// COLORIR PIXEL'S
